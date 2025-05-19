fb2878043e52250b446e10ac1f50d6e43229d2b1
dancan <dancan.morara@student.moringaschool.com>
2025-05-19 22:58:42 +0300

    order

--- shop.py
+++ shop.py
@@ -25,3 +25,35 @@ class Coffee:
     @property
     def name(self):
         return self._name
+    
+class Order:
+    _all_orders = []
+
+    def __init__(self, customer, coffee, price):
+        if not isinstance(customer, Customer):
+            raise TypeError("customer must be an instance of Customer")
+        self._customer = customer
+
+        if not isinstance(coffee, Coffee):
+            raise TypeError("coffee must be an instance of Coffee")
+        self._coffee = coffee
+
+        if not isinstance(price, float):
+            raise TypeError("Price must be a float")
+        if not (1.0 <= price <= 10.0):
+            raise ValueError("Price must be between 1.0 and 10.0")
+        self._price = price
+        
+        Order._all_orders.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
